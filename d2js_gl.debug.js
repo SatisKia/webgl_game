a0c5dd1f712d85a0eb92b2b8077c47e978d7a8ed
SatisKia <satis@msd.biglobe.ne.jp>
2026-07-18 12:14:33 +0900

    fix

--- d2js_gl.debug.js
+++ d2js_gl.debug.js
@@ -1162,6 +1162,12 @@ function _GLStereo( x, y, width, height ){
 	this._position_x = 0.0;
 	this._position_y = 0.0;
 	this._position_z = 0.0;
+	this._look_x = 0.0;
+	this._look_y = 0.0;
+	this._look_z = 0.0;
+	this._up_x = 0.0;
+	this._up_y = 1.0;
+	this._up_z = 0.0;
 }
 _GLStereo.prototype = {
 	setProjectionMatrix : function( mat ){
@@ -1179,6 +1185,12 @@ _GLStereo.prototype = {
 		this._position_x = _glu.positionX();
 		this._position_y = _glu.positionY();
 		this._position_z = _glu.positionZ();
+		this._look_x = _glu.lookX();
+		this._look_y = _glu.lookY();
+		this._look_z = _glu.lookZ();
+		this._up_x = _glu.upX();
+		this._up_y = _glu.upY();
+		this._up_z = _glu.upZ();
 	},
 	clear : function( mask ){
 		_gl.enable( _gl.SCISSOR_TEST );
@@ -1209,34 +1221,32 @@ _GLStereo.prototype = {
 			_glu.viewport( x + width / 2, y, width / 2, height );
 		}
 	},
+	_setMatrix : function( angle ){
+		var a = _glu.deg2rad( angle );
+		var c = Math.cos( a );
+		var s = Math.sin( a );
+		var dx = this._position_x - this._look_x;
+		var dz = this._position_z - this._look_z;
+		var x = this._look_x + dx * c - dz * s;
+		var y = this._position_y;
+		var z = this._look_z + dx * s + dz * c;
+		_glu.lookAt( x, y, z, this._look_x, this._look_y, this._look_z, this._up_x, this._up_y, this._up_z );
+		glStereoSetViewMatrix( _gl, _glu.glMatrix() );
+		_glu.setViewMatrix();
+		_glu.setIdentity();
+		_glu.multiply( _glu.viewMatrix() );
+		_glu.translate( x, y, z );
+		_glu.transpose();
+		_glu.setLookMatrix();
+	},
 	draw : function(){
 		if( this._view_mat != null ){
-			_glu.setIdentity();
-			_glu.rotate( -this._angle, 0.0, 1.0, 0.0 );
-			_glu.multiply( _glu.utMatrix( this._view_mat ) );
-			glStereoSetViewMatrix( _gl, _glu.glMatrix() );
-			_glu.setViewMatrix();
-			_glu.setIdentity();
-			_glu.rotate( this._angle, 0.0, 1.0, 0.0 );
-			_glu.multiply( _glu.viewMatrix() );
-			_glu.translate( this._position_x, this._position_y, this._position_z );
-			_glu.transpose();
-			_glu.setLookMatrix();
+			this._setMatrix( -this._angle );
 		}
 		this._left = true;
 		glStereoDraw( _gl, _glu, this._left );
 		if( this._view_mat != null ){
-			_glu.setIdentity();
-			_glu.rotate( this._angle, 0.0, 1.0, 0.0 );
-			_glu.multiply( _glu.utMatrix( this._view_mat ) );
-			glStereoSetViewMatrix( _gl, _glu.glMatrix() );
-			_glu.setViewMatrix();
-			_glu.setIdentity();
-			_glu.rotate( -this._angle, 0.0, 1.0, 0.0 );
-			_glu.multiply( _glu.viewMatrix() );
-			_glu.translate( this._position_x, this._position_y, this._position_z );
-			_glu.transpose();
-			_glu.setLookMatrix();
+			this._setMatrix( this._angle );
 		}
 		this._left = false;
 		glStereoDraw( _gl, _glu, this._left );
@@ -1866,6 +1876,12 @@ function _GLUtility(){
 	this.position_x = 0.0;
 	this.position_y = 0.0;
 	this.position_z = 0.0;
+	this.look_x = 0.0;
+	this.look_y = 0.0;
+	this.look_z = 0.0;
+	this.up_x = 0.0;
+	this.up_y = 1.0;
+	this.up_z = 0.0;
 	this.look_side = new Array( 3 );
 	this.look_mat = new Array( 16 );
 	this.view_mat = new Array( 16 );
@@ -2225,6 +2241,12 @@ _GLUtility.prototype = {
 		this.position_x = position_x;
 		this.position_y = position_y;
 		this.position_z = position_z;
+		this.look_x = look_x;
+		this.look_y = look_y;
+		this.look_z = look_z;
+		this.up_x = up_x;
+		this.up_y = up_y;
+		this.up_z = up_z;
 		look_x -= this.position_x;
 		look_y -= this.position_y;
 		look_z -= this.position_z;
@@ -2513,6 +2535,24 @@ _GLUtility.prototype = {
 	},
 	positionZ : function(){
 		return this.position_z;
+	},
+	lookX : function(){
+		return this.look_x;
+	},
+	lookY : function(){
+		return this.look_y;
+	},
+	lookZ : function(){
+		return this.look_z;
+	},
+	upX : function(){
+		return this.up_x;
+	},
+	upY : function(){
+		return this.up_y;
+	},
+	upZ : function(){
+		return this.up_z;
 	},
 	projectX : function(){
 		return this.project_x;
